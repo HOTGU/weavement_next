6a7324c056436f92542781e79e1df2b05f5e4758
hangu <gksrn42@gmail.com>
2023-02-28 18:49:45 +0900

    footer

--- components/config/Footer.jsx
+++ components/config/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Logo from "../../public/logo.webp";
-import { Phone } from "react-feather";
+import { Mail, Phone } from "react-feather";
 import Image from "next/legacy/image";
 
 const Footer = () => {
@@ -13,22 +13,23 @@ const Footer = () => {
               <Image src={Logo} placeholder="blur" objectFit="contain" />
             </div>
             <div className="text-xs text-center w-fit md:text-sm sm:text-start">
-              <div>이메일: contact@weavement.co.kr</div>
               <div>서울시 강서구 양천로 738 한강G트리타워, 711호</div>
               <div>사업자등록번호 313-47-00901</div>
             </div>
           </div>
-          <div>
-            <div className="flex items-center text-2xl font-bold gap-2">
-              <Phone />
+          <div className="flex flex-col justify-center items-center md:items-start gap-0 md:gap-1">
+            <div className="flex items-center text-xl md:text-2xl lg:text-3xl font-bold gap-1 md:gap-2">
+              <Phone size={20} />
               <div>010 . 6803 . 7181</div>
             </div>
-            <div className=" text-sm mt-0 sm:mt-3 text-center ">
-              영업시간 9:00~18:00 | 토,일 휴무
+            <div className="flex items-center gap-1 md:gap-2 text-sm md:text-base lg:text-lg">
+              <Mail size={15} />
+              <div>contact@weavement.co.kr</div>
             </div>
+            <div className=" text-sm ">영업시간 9:00~18:00 | 토,일 휴무</div>
           </div>
         </div>
-        <div className="text-center text-xs text-gray-400">
+        <div className="text-center text-xs text-gray-400 mb-1">
           © 2022 WEAVEMENT CO.LTD. ALL RIGHTS RESERVED.
         </div>
       </div>
